b772b06372b494ece007bfdb06a60bebba44af47
J1-js <shinjayw@gmail.com>
2020-11-03 16:47:06 -0500

    First exercise on multi word input

---
+++ ProceduralBasics/multiWord.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace multiWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input several seperated words");
+            string userInput = Console.ReadLine();
+            for (int i = 0; i < userInput.Length; i++) {
+                if(i == 0 && !Char.IsWhiteSpace(userInput[i])) {
+                    Console.Write(userInput[i]);
+                }
+                if (Char.IsWhiteSpace(userInput[i]) && !Char.IsWhiteSpace(userInput[i + 1])){
+                    Console.Write(userInput[i+1]);
+                }
+            }
+        }
+    }
+}
