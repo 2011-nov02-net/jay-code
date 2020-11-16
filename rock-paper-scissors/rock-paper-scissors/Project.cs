87eca63414f1110101a916dc05569c8c63f2bf79
nodeJayS <shinjayw@gmail.com>
2020-11-16 10:57:04 -0500

    11-16 sql assignment

--- rock-paper-scissors/rock-paper-scissors/Project.cs
+++ rock-paper-scissors/rock-paper-scissors/Project.cs
@@ -28,9 +28,9 @@ namespace rock_paper_scissors
 
         private static List<string> CompChoice = new List<string>{"ROCK", "PAPER", "SCISSORS"};
 
-        private static int playerScore;
+        private static int playerScore = 0;
 
-        private static int computerScore;
+        private static int computerScore = 0;
 
         public static void GameRound(string player) {
             var playerIndex = 0;
@@ -48,6 +48,7 @@ namespace rock_paper_scissors
             var random = new Random();
             var compIndex = random.Next(CompChoice.Count);
             var combinedIndex = (playerIndex, compIndex);
+            
             var resultScreen = " " + $"Computer picked {CompChoice[compIndex]}." + " " + $"Player: {playerScore} vs Computer: {computerScore}" + " ";
 
             switch (combinedIndex) {    // (player choice, computer choice)
@@ -59,14 +60,14 @@ namespace rock_paper_scissors
                 case (0, 2):    // rock, scissors
                 case (1, 0):    // paper, scissors
                 case (2, 1):    // scissors, paper
-                    Console.Write("You win!" + resultScreen);
                     playerScore++;
+                    Console.Write("You win!" + resultScreen);
                     break;
                 case (0, 1):    // rock, paper
                 case (1, 2):    // paper, scissors
                 case (2, 0):    // scissors, rock
-                    Console.Write("You lose..." + resultScreen);
                     computerScore++;
+                    Console.Write("You lose..." + resultScreen);
                     break;
             }
         }
